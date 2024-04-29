3d23265498a4cb7d6a4d46e7d4bb08ba14b34151
Yan Alexandre <61830805+YanAlexandrebz@users.noreply.github.com>
2024-04-29 16:53:18 -0300

    Update aladin.py

--- aladin.py
+++ aladin.py
@@ -39,7 +39,8 @@ def generate_release_notes(changes):
 changes = [
     "Corrigido bug na função de login",
     "Adicionada nova funcionalidade de busca",
-    "Atualizada documentação do projeto"
+    "Atualizada documentação do projeto",
+    "Atualizada da tela de cadastro"
 ]
 
 relevant_changes = generate_release_notes(changes)
