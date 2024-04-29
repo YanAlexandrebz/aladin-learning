d6df9949aa0e906dc236e7a157f95b8d7da55c3d
Yan Alexandre <61830805+YanAlexandrebz@users.noreply.github.com>
2024-04-29 17:07:35 -0300

    Create changes.py

---
+++ changes.py
@@ -0,0 +1,6 @@
+changes = [
+    "Corrigido bug na função de login",
+    "Adicionada nova funcionalidade de busca",
+    "Atualizada documentação do projeto",
+    "Atualizada da tela de cadastro"
+]
